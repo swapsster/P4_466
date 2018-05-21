d0975ba3fe50e51b7fe312ab73fbd6f908ee5e55
Søren MV <SorenMV@users.noreply.github.com>
2018-05-21 15:09:45 +0200

    Update classification.m

--- data/classification.m
+++ data/classification.m
@@ -1,19 +1,23 @@
 clear;clc;close all
 
-% Features exstracted from training data
+% Load features exstracted from training data
 features = readtable('features.dat');
 
-% Official classification
+% Load Official classification
 official = readtable('Official.xlsx');
 
+% Save fish names
 c = categorical(official{:,2});
 
+% Make index vectors of official data
 notch_index = logical(official{:, 4});
 
 skin_index = logical(official{:, 7});
 meat_index = ~skin_index;
 
 deform_index = logical(official{:, 6});
+
+excessive_skin_index = logical(official{:, 5});
 
 
 
@@ -34,6 +38,8 @@ hold on
 
 scatter(skin_s_value, skin_v_value)
 scatter(meat_s_value, meat_v_value)
+refline([-10 1545]) % (y = m*x + b) (10*x + y >= 1545) -> meat side
+
 title('S,V channel values from Skin and Meat side')
 xlabel('Satuation')
 ylabel('Value')
@@ -45,6 +51,7 @@ legend('Skin side', 'Meat side', 'Location','northwest')
 % Save all features where notches was set to 1
 notches = features{notch_index, 6};
 %not_notches = features{~notch_index, 6};
+max_notch_area = min(notches);
 
 figure
 set(gcf, 'Position', [700 0 900 400])
@@ -56,9 +63,8 @@ for i=1:length(notch_index)
         b.CData(i,:) = [0.8510 0.3294 0.1020];
     end
 end
-y_max = min(notches);
 xlim = get(gca, 'xlim');
-plot(xlim,[y_max y_max])
+plot(xlim,[max_notch_area max_notch_area])
 title('Comparison of notch area')
 ylabel('Notch area [pixels^2]')
 legend('Fillets without notches')
@@ -67,19 +73,50 @@ legend('Fillets without notches')
 
 good_convexity = features{~deform_index, 7};
 bad_convexity = features{deform_index, 7};
+min_convexity = max(bad_convexity);
 
 figure
+set(gcf, 'Position', [700 400 900 400])
 b = bar(c, features{:,7});
 hold on
 b.FaceColor = 'flat';
 for i=1:length(deform_index)
     if(deform_index(i))
-        b.CData(i,:) = [0.8510 0.3294 0.1020];
+        b.CData(i,:) = [0.8510 0.3294 0.1020]; % red
+    elseif(features{:,7}(i) < min_convexity)
+        b.CData(i,:) = [0.8000 0.8000 0.8000]; % grey
     end
 end
-y_max = min(bad_convexity);
+
 xlim = get(gca, 'xlim');
-plot(xlim,[y_max y_max])
+plot(xlim,[min_convexity min_convexity])
 title('Comparison of convexity')
 ylabel('Convexity')
-legend('Fillets with normal shape')
+legend('Fillets with normal shape','Location','northoutside')
+
+%% Excessive skin
+
+good_amount_skin = features{~excessive_skin_index, 8};
+bad_amount_skin = features{excessive_skin_index, 8};
+max_skin_area = min(bad_amount_skin);
+
+figure
+set(gcf, 'Position', [0 400 900 400])
+b = bar(c, features{:,8});
+hold on
+b.FaceColor = 'flat';
+for i=1:length(excessive_skin_index)
+    if(excessive_skin_index(i))
+        b.CData(i,:) = [0.8510 0.3294 0.1020]; % red
+    end
+end
+
+xlim = get(gca, 'xlim');
+plot(xlim,[max_skin_area max_skin_area])
+title('Comparison of Excessive skin area')
+ylabel('Excessive skin area [pixels^2]')
+legend('Fillets without excessive skin', 'Skin area limit','Location','northoutside')
+
+%% Save limits
+
+fprintf('double max_notch_area = %.1f;\ndouble min_convexity = %.5f;\nint max_skin_area = %d;\n',max_notch_area,min_convexity,max_skin_area);
